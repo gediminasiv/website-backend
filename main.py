9d4508092523a461e4b1ea098db68b5436973581
gediminasiv <gediminasiv>
2019-04-29 17:05:20 +0300

    Add extended website version

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import datetime
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -5,17 +6,41 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    title = 'Pagrindinis'
+    time = datetime.datetime.now()
+
+    return render_template(
+        "index.html",
+        title=title,
+        time=time
+    )
 
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    title = 'Apie mane'
+    return render_template(
+        "about.html",
+        title=title,
+        time=time
+    )
 
 
 @app.route("/portfolio")
 def portfolio():
-    return render_template("portfolio.html")
+    title = 'Mano portfolio'
+
+    portfolio_items = [
+        {'period': '2018-2019', 'job': 'Mechanic'},
+        {'period': '2017-2018', 'job': '-'},
+        {'period': '2016-2017', 'job': 'Big businessman'},
+    ]
+
+    return render_template(
+        "portfolio.html",
+        title=title,
+        items=portfolio_items
+    )
 
 
 if __name__ == '__main__':
